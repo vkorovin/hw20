c44796318cc9542734686b68682ea8be0d44d522
Vasily Korovin <vskorovin@gmail.com>
2023-07-14 17:30:18 +0300

    Celery example

--- server/apps/main/views.py
+++ server/apps/main/views.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .forms import PersonForm
 import server.apps.main.tasks as tasks
-
+import time
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -21,8 +21,14 @@ def get_person(request):
         if person.is_valid():
             url = person.cleaned_data["url"]
             result = tasks.scarper.delay(url)
+
+            time.sleep(10)
+            data = result.get()
+
+            return render(request, "main/view.html", {'data': data})
     else:
         person = PersonForm()
-
     return render(request, "main/form.html", {'form': person})
 
+#def view_person(request: HttpRequest) -> HttpResponse:
+#    return render(request, 'main/view.html', {'data': data})
