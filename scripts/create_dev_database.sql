c4e3fadf3a366f82f55fd5ba91c89795ea112048
Vasily Korovin <vskorovin@gmail.com>
2023-07-14 07:38:31 +0300

    Initial commit

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER hw20 SUPERUSER;
+CREATE DATABASE hw20 OWNER hw20 ENCODING 'utf-8';
